51af1b9afc0f6e9c2624bbc60d7dc626b4e97652
sjuhan123 <sjuhan123@gmail.com>
2023-10-02 20:07:30 +0900

    design #132: LocationSetting 데스크탑 뷰 대응 css 수정

--- fe/src/components/LocationSetting/LocationButtonList/style.ts
+++ fe/src/components/LocationSetting/LocationButtonList/style.ts
@@ -4,4 +4,12 @@ export const ButtonBox = styled.div`
   display: flex;
   justify-content: center;
   gap: 1vw;
+
+  @media (max-width: 768px) {
+    flex-direction: row;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: column;
+  }
 `;
